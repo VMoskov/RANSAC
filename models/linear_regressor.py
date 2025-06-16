3d2818e1410ba0922bf8af6d41608742fe47bc76
VMoskov <vedran.moskov@fer.hr>
2025-06-16 21:10:03 +0200

    added simple linear regressor model

---
+++ models/linear_regressor.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+class LinearRegressor:
+    def __init__(self, num_features):
+        self.W = np.random.randn(num_features + 1)  # +1 for bias term
+
+    def predict(self, X):
+        # X is augmented
+        return X @ self.W
+
+    def fit(self, X, y):
+        # X is augmented
+        self.weights = np.linalg.inv(X.T @ X) @ X.T @ y
